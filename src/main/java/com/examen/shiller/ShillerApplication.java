f5316f0c96485f7e89f7e688bf4f4a4e23039ca9
Xavier <jperez@oeste.mx>
2020-07-24 19:52:44 -0500

    first commit

---
+++ src/main/java/com/examen/shiller/ShillerApplication.java
@@ -0,0 +1,13 @@
+package com.examen.shiller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShillerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examen/shiller/ShillerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examen.shiller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShillerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
